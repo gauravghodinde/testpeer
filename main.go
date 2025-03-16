fb86ca747d8b464eb2d37bea387cd534ff115b23
gauravghodinde <gauravghodinde@gmail.com>
2025-03-17 02:28:32 +0530

    test 5

--- main.go
+++ main.go
@@ -2,33 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
-
-	"github.com/schollz/peerdiscovery"
+	"net"
 )
 
 func main() {
-	fmt.Println("🔍 Starting peer discovery...")
+	interfaces, _ := net.Interfaces()
 
-	discoveries, err := peerdiscovery.Discover(peerdiscovery.Settings{
-		Limit:     -1,               // Find unlimited peers
-		TimeLimit: 30 * time.Second, // Run for 30 seconds
-		Delay:     2000,             // Wait 2 seconds before retrying
-		AllowSelf: false,            // Allow self-discovery (for debugging)
-	})
-
-	if err != nil {
-		log.Fatalf("❌ Error discovering peers: %v", err)
-	}
-
-	// Check if no peers were found
-	if len(discoveries) == 0 {
-		fmt.Println("⚠️ No peers found. Check firewall, AP Isolation, and network settings.")
-	} else {
-		// Print discovered peers
-		for _, d := range discoveries {
-			fmt.Printf("✅ Discovered peer: %s\n", d.Address)
+	for _, i := range interfaces {
+		addrs, _ := i.Addrs()
+		for _, addr := range addrs {
+			fmt.Println("🔍 Found device IP:", addr.String())
 		}
 	}
 }
